65e71bc2535b6e20a66705e76aabda32e73f9c73
Jose Bustamante <bercopy@gmail.com>
2020-07-26 02:27:49 +0200

    Coverage command fix

--- src/components/InputSearch/InputSearch.stories.js
+++ src/components/InputSearch/InputSearch.stories.js
@@ -23,86 +23,41 @@ export const resultsSimple = [
   { searchterm: "calvin klein trui heren oranje", nrResults: 42 },
 ];
 
-storiesOf("InputSearch", module)
-  .add("Default", () => {
-    class StoryComp extends React.Component {
-      state = {
-        results: [],
-        isLoading: false,
-      };
+storiesOf("InputSearch", module).add("Default", () => {
+  class StoryComp extends React.Component {
+    state = {
+      results: [],
+      isLoading: false,
+    };
 
-      // Fake a async fetch
-      getResults(input) {
-        this.setState({ ...this.state, isLoading: true });
-        setTimeout(() => {
-          if (input === "none") this.setState({ results: [] });
-          else this.setState({ results: resultsSimple, isLoading: false });
-        }, 1000);
-      }
-
-      render() {
-        return (
-          <Container>
-            <InputSearch
-              id="exampleId"
-              placeholder="Search"
-              label="Organization"
-              notFoundMessage="Unable to find organization"
-              minChars={3}
-              debounceTimeout={500}
-              onSearch={(input) => this.getResults(input)}
-              onSelectResult={action("Item Selected")}
-              searchResults={this.state.results}
-              isLoading={this.state.isLoading}
-            />
-          </Container>
-        );
-      }
+    // Fake a async fetch
+    getResults(input) {
+      this.setState({ ...this.state, isLoading: true });
+      setTimeout(() => {
+        if (input === "none") this.setState({ results: [] });
+        else this.setState({ results: resultsSimple, isLoading: false });
+      }, 1000);
     }
 
-    return <StoryComp />;
-  })
-  .add("With Error", () => {
-    class StoryComp extends React.Component {
-      state = {
-        results: [],
-        error: false,
-      };
-
-      // Fake a async fetch
-      getResults(input) {
-        setTimeout(() => {
-          if (input === "none") this.setState({ results: [] });
-          else this.setState({ results: resultsSimple });
-        }, 400);
-      }
-
-      render() {
-        return (
-          <Container>
-            <InputSearch
-              id="exampleId"
-              placeholder="Search"
-              label="Organization"
-              notFoundMessage="Unable to find organization"
-              minChars={3}
-              debounceTimeout={500}
-              onGetResults={(input) => this.getResults(input)}
-              onSelectResult={() => this.setState({ error: true })}
-              searchResults={this.state.results}
-              error={this.state.error}
-              errorMessage="Company already registered"
-              errorTooltip={
-                <p>
-                  It seems like you company is already registered. Go to login
-                  screen or contact customer support.
-                </p>
-              }
-            />
-          </Container>
-        );
-      }
+    render() {
+      return (
+        <Container>
+          <InputSearch
+            id="exampleId"
+            placeholder="Search"
+            label="Organization"
+            notFoundMessage="Unable to find organization"
+            minChars={3}
+            debounceTimeout={500}
+            onSearch={(input) => this.getResults(input)}
+            onSelectResult={action("Item Selected")}
+            searchResults={this.state.results}
+            isLoading={this.state.isLoading}
+          />
+        </Container>
+      );
     }
+  }
 
-    return <StoryComp />;
-  });
+  return <StoryComp />;
+});
